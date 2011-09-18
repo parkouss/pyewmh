775c813afc167e2a3bc4fbe1276cdfa532629db6
parcouss <parcouss@85d90e14-da94-43f5-9830-2b8616a56d0e>
2011-09-18 12:41:55 +0000

    adding setup.py
    git-svn-id: svn://svn.code.sf.net/p/pyewmh/code/trunk@3 85d90e14-da94-43f5-9830-2b8616a56d0e

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='ewmh',
+      version='0.1',
+      description='python implementation of Extended Window Manager Hints, based on Xlib',
+      author='parcouss',
+      url='http://pyewmh.sourceforge.net/',
+      py_modules=['ewmh'],
+      )

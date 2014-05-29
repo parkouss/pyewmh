229cbf23ae738bdd27fa829c54b022880a6b99d7
Julien Pagès <j.parkouss@gmail.com>
2014-05-29 04:42:19 +0200

    add working Xlib dependency for python3

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+import sys
+
+if sys.version > '3':
+    install_requires = ['python3-xlib']
+else:
+    # I can not specify Xlib as a dependency now as there is no
+    # release for python2 on PyPi
+    install_requires = []
 
 setup(name='ewmh',
       version='0.1',
@@ -11,6 +19,6 @@ setup(name='ewmh',
       author_email="j.parkouss@gmail.com",
       url='https://github.com/parkouss/pyewmh',
       py_modules=['ewmh'],
-      install_requires=['Xlib'],
+      install_requires=install_requires,
       license='LICENSE.txt',
 )

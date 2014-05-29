d74bde7590792699b44c36c5d536f03ea808c220
Julien Pagès <j.parkouss@gmail.com>
2014-05-29 03:45:43 +0200

    adding usefull infos to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 setup(name='ewmh',
       version='0.1',
       description='python implementation of Extended Window Manager Hints, based on Xlib',
-      author='parcouss',
-      url='http://pyewmh.sourceforge.net/',
+      long_description=open('README').read(),
+      author='parkouss',
+      author_email="j.parkouss@gmail.com",
+      url='https://github.com/parkouss/pyewmh',
       py_modules=['ewmh'],
-      )
+      install_requires=['Xlib'],
+      license='LICENSE.txt',
+)

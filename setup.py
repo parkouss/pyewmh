2abcb99a5e10f35550ffc67cc71a311b59d44c55
Julien Pagès <j.parkouss@gmail.com>
2016-12-21 14:19:53 +0100

    pep8 fixes

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-import sys, re
+import sys
+import re
+
 
 if sys.version_info < (2, 6):
     sys.exit("ewmh >= 0.1.3 requires python >= 2.6")
 
 setup(name='ewmh',
-      version=re.findall("__version__ = '(.+)'", open('ewmh/__init__.py').read())[0],
-      description='python implementation of Extended Window Manager Hints, based on Xlib',
+      version=re.findall("__version__ = '(.+)'",
+                         open('ewmh/__init__.py').read())[0],
+      description=(
+          'python implementation of Extended Window Manager Hints, based on'
+          ' Xlib'
+      ),
       long_description=open('README.rst').read(),
       author='parkouss',
       author_email="j.parkouss@gmail.com",
@@ -21,5 +27,4 @@ setup(name='ewmh',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 3',
           'Intended Audience :: Developers',
-      ),
-)
+      ))
